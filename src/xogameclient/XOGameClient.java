02a145002297cf3eb69a47aadb96aab8995a39f8
esraakhaled <www.esraa.khaled.fciscusuez@gmail.com>
2021-12-23 18:41:26 +0200

    second

--- src/xogameclient/XOGameClient.java
+++ src/xogameclient/XOGameClient.java
@@ -19,7 +19,7 @@ public class XOGameClient extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = new RegisterBase() ;
         
         Scene scene = new Scene(root);
         
