9289c317f400592d7e59007a6031763073bd0885
Ahmed Mohamed Abdallah <benten12536.com>
2022-01-01 12:20:20 +0200

    saving data

--- src/model/PcGame.java
+++ src/model/PcGame.java
@@ -3,20 +3,124 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package model;
 
+
+import java.util.Random;
 import javafx.scene.control.Button;
 
 /**
  *
  * @author ahmed
  */
-public class PcGame extends LocalGame{
-   
+public class PcGame extends LocalGame{ 
+    
  public PcGame() {
      super("PlayerA","BOOT");
        
     }
 
-  
+ //function playing with computer
+
+    public int[] computerPlay() {
+        int c= new Random().nextInt(3); 
+        int v= new Random().nextInt(3);  
+       
+        while(!isFound(c, v)) {
+            c = new Random().nextInt(3);  ;
+            v = new Random().nextInt(3);  ; 
+        }
+         int arr[] = {c, v};
+        System.out.println(c+" , " +v);
+         return arr;
+
+    }
+    //function check whether x or o is on board
+
+    public boolean isFound(int x, int y) {
+        return (board[x][y] != X && board[x][y] != O);
+    }
+    //function return button in which player computer is playing
+
+    public Button playerB() {
+        Button d = new Button();
+        int arr[] = computerPlay();
+        int x = arr[0];
+        int y = arr[1];
+        if ((x == 0) && (y == 0)) {
+            d = buttons.get(0);
+        } else if ((x == 0) && (y == 1)) {
+            d = buttons.get(1);
+        } else if ((x == 0) && (x == 2)) {
+            d = buttons.get(2);
+        } else if ((x == 1) && (y == 0)) {
+            d = buttons.get(3);
+        } else if ((x == 1) && (y == 1)) {
+            d = buttons.get(4);
+        } else if ((x == 1) && (y == 2)) {
+            d = buttons.get(5);
+        } else if ((x == 2) && (y == 0)) {
+            d = buttons.get(6);
+        } else if ((x == 2) && (y == 1)) {
+            d = buttons.get(7);
+        } else if ((x == 2) && (y == 2)) {
+            d = buttons.get(8);
+        } else {
+
+            System.out.println("out of bound");
+        }
+
+        return d;
+    }
+
+    @Override
+    public void handleButton(Button b) {
+
+        String cellPostion = b.getId();
+        b.setDisable(true);
+
+        actionPlayerA(cellPostion);
+        b.setText("X");
+        
+        if (!winningCases(X)) {
+            
+        } else  {
+            disableButtons();
+            winner = playerA.getUserName();
+            playerA.setScore(playerA.getScore() + 1);
+            update();
+            // endbale back
+            enableBack();
+            enableRestart();
+            if(endGame()) {
+                enableBack();
+                enableRestart();
+
+            }
+        }
+        Button buttonB=null;
+        buttonB = playerB();
+        buttonB.setDisable(true);
+        actionPlayerB(buttonB.getId());
+        buttonB.setText("O");
+        
+        if (!winningCases(O)) {
+
+        } else {
+            disableButtons();
+            winner = playerB.getUserName();
+            playerB.setScore(playerB.getScore() + 1);
+            update();
+            enableBack();
+            enableRestart();
+
+        }
+
+    }
+
 }
+
+
+  
+
--- src/xogameclient/GameScreen.java
+++ src/xogameclient/GameScreen.java
@@ -524,10 +524,9 @@ public class GameScreen extends BorderPane {
         g.addRestart(restartButton);
 
         cell_0_0.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
-            if(g instanceof LocalGame){
                         g.handleButton(cell_0_0);
 
-            }
+            
 
 
         });
@@ -599,13 +598,12 @@ public class GameScreen extends BorderPane {
 
     public void addButtons() {
         buttons.add(cell_0_0);
-        buttons.add(cell_2_0);
-        buttons.add(cell_1_2);
-        buttons.add(cell_1_1);
-        buttons.add(cell_1_0);
         buttons.add(cell_0_1);
         buttons.add(cell_0_2);
-        buttons.add(cell_0_1);
+        buttons.add(cell_1_0);
+        buttons.add(cell_1_1);
+        buttons.add(cell_1_2);
+        buttons.add(cell_2_0);
         buttons.add(cell_2_1);
         buttons.add(cell_2_2);
     }
